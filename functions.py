f3f2014f0c7617e6bf09f8f32de2dd146f7a8da6
nkellyulster <162691993+nkellyulster@users.noreply.github.com>
2024-04-24 11:44:48 +0100

    Updated functions. Still need to be commented.

--- functions.py
+++ functions.py
@@ -1,8 +1,8 @@
 # The department of Education dataset is poorly formatted and lots of columns
 # have leading and trailing whitespace. This fuction address this issue.
-def remove_whitespace(merged_data):
-    merged_data.columns = merged_data.columns.str.strip()
-    return merged_data
+# def remove_whitespace(merged_data):
+#     merged_data.columns = merged_data.columns.str.strip()
+#     return merged_data
 
 #Function for converting kilometers to miles
 def km_to_miles(kilometers):
@@ -10,6 +10,12 @@ def km_to_miles(kilometers):
     return miles
       
 # Function to convert distance to area
-def convert_distance_to_area(distance):
-    global merged_data
-    return merged_data[distance] ** 2 * math.pi
+def convert_distance_to_area(row, distance_column):
+    return row[distance_column] ** 2 * math.pi
+
+# Function to apply the conditional sustainability logic
+def sustainable_schools(row):
+    if (row['Urban/ Rural     '] == "RURAL" and row['total enrolment'] < 105) or (row['Urban/ Rural     '] == "URBAN" and row['total enrolment'] < 140):
+        return 'Not Sustainable'
+    else:
+        return 'Sustainable'
